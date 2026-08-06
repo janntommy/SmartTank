b4eb01555f1ea5f32a2b345adaccc78aa1075c3b
janntommy <tomekjanisiewi@gmail.com>
2026-08-06 15:01:48 +0200

    fix downloading nbp_eur

--- src/download/download_nbp_eur.py
+++ src/download/download_nbp_eur.py
@@ -1,35 +1,38 @@
 import requests
 import json
 from pathlib import Path
-from datetime import datetime, timedelta
+from datetime import datetime
+import calendar
 
-def download_nbp_eur(days: int = 90):
-    start_date = (datetime.now() - timedelta(days=days)).strftime('%Y-%m-%d')
-    end_date = datetime.now().strftime('%Y-%m-%d')
 
-    url = f"http://api.nbp.pl/api/exchangerates/rates/a/eur/{start_date}/{end_date}/?format=json"
-    response = requests.get(url)
+def download_nbp_eur(year: int = 2026):
+    project_root = Path(__file__).resolve().parents[2]
+    dir = project_root / "data" / "raw" / "nbp_eur"
+    dir.mkdir(parents=True, exist_ok=True)
 
-    if response.status_code == 200:
-        data = response.json()
+    current_month = datetime.now().month
 
-        script_dir = Path(__file__).parent.resolve()
-        dir = script_dir.parent.parent / "data" / "raw" / "nbp_eur"
-        dir.mkdir(parents=True, exist_ok=True)
+    for month in range(1, current_month + 1):
+        last_day = calendar.monthrange(year, month)[1]
 
-        file_path = dir / f"nbp_eur_raw_{end_date.replace("-", "_")}.json"
+        if month == current_month:
+            last_day = datetime.now().day
 
-        if file_path.exists():
-            print("Nbp_eur file already exists")
-        else:
-            with open(file_path, 'w') as file:
+        start_date = f"{year}-{month:02d}-01"
+        end_date = f"{year}-{month:02d}-{last_day:02d}"
+
+        url = f"http://api.nbp.pl/api/exchangerates/rates/a/eur/{start_date}/{end_date}/?format=json"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            file_name = f"nbp_eur_{year}_{month:02d}.json"
+            file_path = dir / file_name
+
+            with open(file_path, 'w', encoding='utf-8') as file:
                 json.dump(response.json(), file, indent=4)
 
-            print("Successfully downloaded nbp_eur file")
-        return file_path
-
-    else:
-        return None
-
+            print(f"Succesfully downloaded 2026-{month:02d}-{last_day:02d} nbp_eur file")
+        else:
+            print("Error, cannot download nbp_eur file")
 if __name__ == "__main__":
     download_nbp_eur()
