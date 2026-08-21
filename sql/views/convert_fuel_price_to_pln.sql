35c1d7f4bd8ff438762a3f4b45b4dc5250530ace
janntommy <tomekjanisiewi@gmail.com>
2026-08-21 15:02:36 +0200

    update price conversion

--- sql/views/convert_fuel_price_to_pln.sql
+++ sql/views/convert_fuel_price_to_pln.sql
@@ -1,6 +1,18 @@
-DROP VIEW IF EXISTS fuel_gold;
+DROP VIEW IF EXISTS fuel_pln;
+
+
 CREATE OR REPLACE VIEW fuel_pln AS
-    SELECT fuel.date, fuel.country, fuel.fuel_type, fuel.price_eur_per_litre, nbp_eur.eur_to_pln_rate, ROUND((fuel.price_eur_per_litre * nbp_eur.eur_to_pln_rate), 2) AS fuel_to_pln
-    FROM fuel
-    INNER JOIN nbp_eur ON fuel.date = nbp_eur.date
-    WHERE fuel.date >= '2026-01-01'
+SELECT fuel.date,
+       fuel.country,
+       fuel.fuel_type,
+       fuel.price_eur_per_litre,
+       n.eur_to_pln_rate,
+       ROUND(fuel.price_eur_per_litre * n.eur_to_pln_rate, 2) AS fuel_to_pln
+FROM fuel
+LEFT JOIN LATERAL (
+    SELECT eur_to_pln_rate
+    FROM nbp_eur
+    WHERE nbp_eur.date <= fuel.date
+    ORDER BY nbp_eur.date DESC
+    LIMIT 1
+) AS n ON true;
